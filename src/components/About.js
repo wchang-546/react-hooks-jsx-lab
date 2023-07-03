8084c292140f3e4e6f961762d5f249b353223918
Will Chang <william.chaang@gmail.com>
2023-07-03 13:29:13 -0600

    Hooks manipulated

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import {image} from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+      <h2> About Me</h2>
+      <p> I am a software engineer based in Boulder </p>
+      <img src={image} alt="I made this"/>
+  </div>
+  )
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const firebrick = {
+  color: 'firebrick'
+}
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+    <h1 style={firebrick}> {name} is a Web Developer from {city} </h1>
+  </div>
+  )
 }
 
 export default Home;
